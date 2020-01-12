ce40a46899cabfb6bbb22ad571fbaeaf8514d3e1
Luca Srdjenovic <luca.srdjenovic@gmail.com>
2020-01-12 01:27:56 +0100

    Add BlockEth type

--- src/types.ts
+++ src/types.ts
@@ -53,3 +53,26 @@ export interface GetBlock {
   merkleroot: string
   tx: string[]
 }
+
+export interface GetBlockEth {
+  difficulty: number
+  extraData: string
+  gasLimit: number
+  gasUsed: number
+  hash: string
+  logsBloom: string
+  miner: string
+  mixHash:string
+  nonce:string
+  number:  number
+  parentHash: string
+  receiptsRoot: string
+  sha3Uncles: string
+  size: number
+  stateRoot: string
+  timestamp:number
+  totalDifficulty: number
+  transactions: string[]
+  transactionsRoot: string
+  uncles: string[]
+}
